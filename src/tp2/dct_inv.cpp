c51cdbf4e427595763f2fb3fda6c0469d6b6b937
Minijackson <minijackson@riseup.net>
2017-10-12 23:03:10 -0400

    Adding discrete cosinus transformations

--- src/tp2/dct_inv.cpp
+++ src/tp2/dct_inv.cpp
@@ -1,7 +1,11 @@
-
 #include "tp2/common.hpp"
 
 using namespace tp2;
+
+// Defined in dct.cpp
+double fast_inverse_sqrt(double x);
+// Defined in dct.cpp
+double c(int w, int n);
 
 // dct_inv: computes the inverse discrete cosinus tranform of a matrix
 cv::Mat_<uchar> tp2::dct_inv(const cv::Mat_<float>& oBlock) {
@@ -9,7 +13,32 @@ cv::Mat_<uchar> tp2::dct_inv(const cv::Mat_<float>& oBlock) {
 	          oBlock.isContinuous());
 	cv::Mat_<uchar> oOutput(oBlock.size());
 
-	// @@@@ TODO (do not use built-in functions here!)
+	for(int x = 0; x < oBlock.rows; ++x) {
+		for(int y = 0; y < oBlock.cols; ++y) {
+
+			// Smiley -------.
+			//               |
+			//               v
+			double value = (0.0);
+			//               ^
+			//               |
+			// Smiley -------'
+
+			for(int u = 0; u < oBlock.rows; ++u) {
+				for(int v = 0; v < oBlock.cols; ++v) {
+
+					double color     = oBlock.at<float>(u, v);
+					double first_cos = std::cos((M_PI * (2.0 * x + 1.0)) * u / (2.0 * oBlock.rows));
+					double second_cos =
+					        std::cos((M_PI * (2.0 * y + 1.0)) * v / (2.0 * oBlock.rows));
+
+					value += c(u, oBlock.rows) * c(v, oBlock.rows) * color * first_cos * second_cos;
+				}
+			}
+
+			oOutput.at<uchar>(x, y) = std::round(value);
+		}
+	}
 
 	return oOutput;
 }
