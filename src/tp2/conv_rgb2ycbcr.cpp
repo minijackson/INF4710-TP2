7513b8c04ad87a0a966b88b7c66bdd035c078f1d
Minijackson <minijackson@riseup.net>
2017-10-13 15:44:54 -0400

    Fix RGB -> YCbCr and fix YCbCr -> RGB

--- src/tp2/conv_rgb2ycbcr.cpp
+++ src/tp2/conv_rgb2ycbcr.cpp
@@ -2,6 +2,18 @@
 
 using namespace tp2;
 
+double rgb2y(uchar r, uchar g, uchar b) {
+	return std::min((0.299 * r) + (0.587 * g) + (0.114 * b), 255.0);
+}
+
+double rgb2cb(uchar r, uchar g, uchar b) {
+	return std::min(128.0 - (0.16878 * r) - (0.3313 * g) + (0.5 * b), 255.0);
+}
+
+double rgb2cr(uchar r, uchar g, uchar b) {
+	return std::min(128.0 + (0.5 * r) - (0.4187 * g) - (0.08131 * b), 255.0);
+}
+
 // conv_rgb2ycbcr: converts an 8-bit-depth RGB image to Y'CbCr format using optional 4:2:0
 // subsampling
 void tp2::conv_rgb2ycbcr(const cv::Mat& RGB,
@@ -10,25 +22,56 @@ void tp2::conv_rgb2ycbcr(const cv::Mat& RGB,
                          cv::Mat_<uchar>& Cb,
                          cv::Mat_<uchar>& Cr) {
 	CV_Assert(!RGB.empty() && RGB.type() == CV_8UC3 && RGB.isContinuous());
+
 	Y.create(RGB.rows, RGB.cols);
 
-	// @@@@ TODO (do not use built-in functions here!)
-	// Cb.create(/*...*/);
-	// Cr.create(/*...*/);
-	// ...
+	if(bSubsample) {
+		Cb.create(RGB.rows / 2, RGB.cols / 2);
+		Cr.create(RGB.rows / 2, RGB.cols / 2);
+	} else {
+		Cb.create(RGB.rows, RGB.cols);
+		Cr.create(RGB.rows, RGB.cols);
+	}
 
-	for(int row = 0; row < RGB.rows; ++row)
-	{
-		for(int col = 0; col < RGB.cols; ++col)
-		{
+	double cb = 0, cr = 0;
+
+	for(int row = 0; row < RGB.rows; ++row) {
+
+		for(int col = 0; col < RGB.cols; ++col) {
 			uint8_t b = RGB.at<cv::Vec3b>(row, col)[0];
 			uint8_t g = RGB.at<cv::Vec3b>(row, col)[1];
 			uint8_t r = RGB.at<cv::Vec3b>(row, col)[2];
 
-			uchar y =0.299*r + 0.587*g + 0.114*b;
+			uchar y = rgb2y(r, g, b);
+
 			Y.at<uchar>(row, col) = y;
-			Cb.at<uchar>(row, col) = 128 + 0.564*(b-y);
-			Cr.at<uchar>(row, col) = 128 + 0.713*(r-y);
+
+			if(bSubsample) {
+
+				if(row % 2 == 0 && col % 2 == 0) {
+					cb = 0;
+					cr = 0;
+					std::cout << "Initialization" << std::endl;
+				}
+
+				cb += rgb2cb(r, g, b);
+				cr += rgb2cr(r, g, b);
+				std::cout << "cb = " << cb << ", cr = " << cr << std::endl;
+
+				if(row % 2 == 1 && col % 2 == 1) {
+					cb /= 4.0;
+					cr /= 4.0;
+
+					std::cout << "Mean: cb = " << cb << ", cr = " << cr << std::endl;
+
+					Cb.at<uchar>(row / 2, col / 2) = std::round(cb);
+					Cr.at<uchar>(row / 2, col / 2) = std::round(cr);
+				}
+
+			} else {
+				Cb.at<uchar>(row, col) = std::round(rgb2cb(r, g, b));
+				Cr.at<uchar>(row, col) = std::round(rgb2cr(r, g, b));
+			}
 		}
 	}
 }
--- src/tp2/conv_ycbcr2rgb.cpp
+++ src/tp2/conv_ycbcr2rgb.cpp
@@ -1,4 +1,3 @@
-
 #include "tp2/common.hpp"
 
 using namespace tp2;
@@ -16,27 +15,30 @@ void tp2::conv_ycbcr2rgb(const cv::Mat_<uchar>& Y,
 	                          Y.cols / 2 == Cb.cols && Y.cols / 2 == Cr.cols));
 	CV_Assert(bSubsample ||
 	          (Y.rows == Cb.rows && Y.rows == Cr.rows && Y.cols == Cb.cols && Y.cols == Cr.cols));
+
 	RGB.create(Y.size(), CV_8UC3);
 
-	// @@@@ TODO (do not use built-in functions here!)
-
-	for(int row = 0; row < RGB.rows; ++row)
-	{
-		for(int col = 0; col < RGB.cols; ++col)
-		{
+	for(int row = 0; row < RGB.rows; ++row) {
+		for(int col = 0; col < RGB.cols; ++col) {
 
 			uchar y = Y.at<uchar>(row, col);
-			uchar cb = Cb.at<uchar>(row, col);
-			uchar cr = Cr.at<uchar>(row, col);
+			uchar cb, cr;
+
+			if(bSubsample) {
+				cb = Cb.at<uchar>(row / 2, col / 2);
+				cr = Cr.at<uchar>(row / 2, col / 2);
+			} else {
+				cb = Cb.at<uchar>(row, col);
+				cr = Cr.at<uchar>(row, col);
+			}
 
 			// Blue
-			RGB.at<cv::Vec3b>(row, col)[0] = y + 1.773*(cb-128);
-
+			RGB.at<cv::Vec3b>(row, col)[0] = std::max(std::round(y + 1.772 * (cb - 128)), 0.0);
 			// Green
-			RGB.at<cv::Vec3b>(row, col)[1] = y - 0.714*(cr-128) - 0.344*(cb-128);
-
+			RGB.at<cv::Vec3b>(row, col)[1] =
+			        std::max(std::round(y - 0.344136 * (cb - 128) - 0.714136 * (cr - 128)), 0.0);
 			// Red
-			RGB.at<cv::Vec3b>(row, col)[2] = y + 1.403*(cr-128);
+			RGB.at<cv::Vec3b>(row, col)[2] = std::max(std::round(y + 1.402 * (cr - 128)), 0.0);
 		}
 	}
 }
